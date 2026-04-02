b18ff8d0f9e09f81decd35f123010f111e986fe1
benmatiko <maroamatiko@gmail.com>
2026-04-02 05:58:35 +0300

    first commit

---
+++ CondTestDemo.java
@@ -0,0 +1,29 @@
+public class CondTestDemo {
+    public static void main(String[] args) {
+
+      int score = 74; 
+      String grade; 
+
+      double gpaPoints;   
+
+      if (score >= 90) {     
+        grade     = "A";     
+        gpaPoints = 4.0; 
+      } else if (score >= 80) {     
+        grade     = "B+";     
+        gpaPoints = 3.5; 
+      } else if (score >= 70) {     
+        grade     = "B";     
+        gpaPoints = 3.0; 
+      } else if (score >= 60) {     
+        grade     = "C+"; 
+        gpaPoints = 2.5; 
+      } else {     
+        grade     = "F";     
+        gpaPoints = 0.0; 
+      } 
+    } 
+  } 
+
+
+  System.out.printf("Score: %d  |  Grade: %s  |  GPA Points: %.1f%n", score, grade, gpaPoints)
